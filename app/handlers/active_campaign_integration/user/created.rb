7dfb62a031d0fe87d8d36d1469a9bce6d5264615
Adam Mikulasev <adammikulas@gmail.com>
2023-06-14 03:17:49 +1000

    add handlers

---
+++ app/handlers/active_campaign_integration/user/created.rb
@@ -0,0 +1,9 @@
+module ActiveCampaignIntegration
+  module User
+    module_function
+
+    def created(message:)
+      # TODO: ActiveCampaign::Contact.create(...)
+    end
+  end
+end
---
+++ app/handlers/mailchimp_integration/user/created.rb
@@ -0,0 +1,9 @@
+module MailchimpIntegration
+  module User
+    module_function
+
+    def created(message:)
+      # TODO: Mailchimp::List.add_member(...)
+    end
+  end
+end
